44463355f924b3641c82cd51c80d42e606468806
Romain <rmurat.murat@gmail.com>
2022-04-19 17:25:27 +0200

    first push

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PHP/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PHP from "@/components/PHP";
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {PHP},
+  mounted() {
+    console.log('App mounted.');
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/PHP.vue
@@ -0,0 +1,42 @@
+<template>
+  <h3>PHP</h3>
+  <div>
+    <label for="version-php">PHP Version</label>
+    <select id="version-php">
+
+    </select>
+  </div>
+  <div>
+    <div>
+      <label for="new-version-php">New Version</label>
+      <select id="new-version-php">
+      </select>
+    </div>
+    <div>
+      <label for="new-subversion-php">SubVersion</label>
+      <select id="new-subversion-php">
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "PHP",
+  created: function() {
+    axios.get('https://www.php.net/releases/index.php?json')
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
