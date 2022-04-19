e36fcb0224176c640f780097581aadd9452ca305
Romain <rmurat.murat@gmail.com>
2022-04-19 17:55:37 +0200

    add api php release

--- src/components/PHP.vue
+++ src/components/PHP.vue
@@ -8,13 +8,15 @@
   </div>
   <div>
     <div>
-      <label for="new-version-php">New Version</label>
-      <select id="new-version-php">
+      <label for="new-version-php">Version</label>
+      <select id="new-version-php" @change="newVersionPHPChange">
+        <option>{{this.selectedVersion}}</option>
       </select>
     </div>
     <div>
       <label for="new-subversion-php">SubVersion</label>
       <select id="new-subversion-php">
+        <option v-for="subversion in subversions" >{{subversion.version}}</option>
       </select>
     </div>
   </div>
@@ -25,15 +27,38 @@ import axios from "axios";
 
 export default {
   name: "PHP",
+  data() {
+    return {
+      versions: {},
+      subversions: {},
+      selectedVersion: "",
+      selectedSubversion: ""
+    };
+  },
   created: function() {
     axios.get('https://www.php.net/releases/index.php?json')
       .then(response => {
-        console.log(response.data);
+        this.versions = response.data;
+
+        this.selectedVersion = Object.keys(this.versions).pop();
       })
       .catch(error => {
         console.log(error)
       })
   },
+  methods:{
+    newVersionPHPChange: function(event){
+      this.selectedVersion = this.versions[event.target.selectedIndex];
+      axios.get('https://www.php.net/releases/index.php?json&version=' + this.selectedVersion)
+        .then(response => {
+          let data = response.data;
+          this.subversions = data[this.selectedVersion];
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }
 }
 </script>
 
